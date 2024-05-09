c055d0537c2677dd17cbb9f401a8dfea9912db04
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-05-09 15:50:28 +0300

    ISAPP-67: using User entity from security

--- src/main/java/vsu/cs/is/infsysserver/security/entity/temp/User.java
+++ src/main/java/vsu/cs/is/infsysserver/security/entity/temp/User.java
@@ -21,6 +21,12 @@ public class User {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     @Column(name = "password")
     private String password;
 
